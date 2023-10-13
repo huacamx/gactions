d7d264da0ba9c8189ad3b351664b7ea1e63b7efb
Hugo Cabrera <hdan.629@gmail.com>
2023-10-13 17:33:41 -0600

    black reformat

--- wa_services/main.py
+++ wa_services/main.py
@@ -1,5 +1,7 @@
+import functions_framework
 
-@functions_framework.http   
+
+@functions_framework.http
 def wa_services(requet):
     print("helloworld")
-    pass                                                                
+    pass
